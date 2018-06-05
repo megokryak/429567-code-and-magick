9f13d8e1d7120b601aae5ec15d6c3cb7fe726fcd
Andrey <XaJIaC@yandex.ru>
2018-06-05 11:17:19 +0300

    Change ))

--- js/stat.js
+++ js/stat.js
@@ -7,6 +7,8 @@ window.renderStatistics = function (ctx, names, times) {
   var CLOUD_START_Y = 10;
   var SHADOW_BIAS = 10;
   var WIDTH_COLUMN = 40;
+  var COLOR_CLOUD = '#fff';
+  var COLOR_SHADOW = 'rgba(0, 0, 0, 0.7)';
   var HEIGHT_BARCHART = 150;
   var DISTANCE_COLUMN = 50;
   var MY_COLOR = 'rgba(255, 0, 0, 1)';
@@ -16,42 +18,22 @@ window.renderStatistics = function (ctx, names, times) {
   var statisticStartY = CLOUD_START_Y + CLOUD_HEIGHT - 35;
   var maxTime;
 
-  renderClound(CLOUD_START_X, CLOUD_START_Y, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_BIAS, ctx);
+  renderClound(CLOUD_START_X + SHADOW_BIAS, CLOUD_START_Y + SHADOW_BIAS, CLOUD_WIDTH, CLOUD_HEIGHT, COLOR_SHADOW, ctx);
+  renderClound(CLOUD_START_X, CLOUD_START_Y, CLOUD_WIDTH, CLOUD_HEIGHT, COLOR_CLOUD, ctx);
   renderText(CLOUD_START_X, CLOUD_START_Y, ctx);
   maxTime = searchMax(times);
   renderStatisticPlayer(statisticStartX, statisticStartY, names, times, HEIGHT_BARCHART, WIDTH_COLUMN, maxTime, MY_COLOR, OTHER_COLOR, DISTANCE_COLUMN, ctx);
 };
 
-var renderClound = function (cloudStartX, cloudStartY, cloudWidth, cloudHeight, shadowBias, ctx) {
+var renderClound = function (cloudStartX, cloudStartY, cloudWidth, cloudHeight, color, ctx) {
 
   var cloudEndX = cloudStartX + cloudWidth;
   var cloudEndY = cloudStartY + cloudHeight;
   var cloudCenterX = (cloudEndX - cloudStartX) / 2 + cloudStartX;
   var cloudCenterY = (cloudEndY - cloudStartY) / 2 + cloudStartY;
-  var shadowStartX = cloudStartX + shadowBias;
-  var shadowStartY = cloudStartY + shadowBias;
-  var shadowCenterX = cloudCenterX + shadowBias;
-  var shadowCenterY = cloudCenterY + shadowBias;
-  var shadowEndX = cloudEndX + shadowBias;
-  var shadowEndY = cloudEndY + shadowBias;
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.beginPath();
-  ctx.moveTo(shadowStartX, shadowStartY);
-  ctx.lineTo(shadowCenterX, shadowStartY + 10);
-  ctx.lineTo(shadowEndX, shadowStartY);
-  ctx.lineTo(shadowEndX - 10, shadowCenterY);
-  ctx.lineTo(shadowEndX, shadowEndY);
-  ctx.lineTo(shadowCenterX, shadowEndY - 10);
-  ctx.lineTo(shadowStartX, shadowEndY);
-  ctx.lineTo(shadowStartX + 10, shadowCenterY);
-  ctx.closePath();
-  ctx.stroke();
-  ctx.fill();
-
-  ctx.fillStyle = '#fff';
-  ctx.strokeStyle = '#fff';
+  ctx.fillStyle = color;
+  ctx.strokeStyle = color;
   ctx.beginPath();
   ctx.moveTo(cloudStartX, cloudStartY);
   ctx.lineTo(cloudCenterX, cloudStartY + 10);
